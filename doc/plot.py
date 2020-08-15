0b7b7cce9d8709d5bb6db6a6dc5b58e98629d607
João Gória <jgoriasilva@gmail.com>
2020-08-14 23:30:19 -0300

    Salvando o trabalho

--- doc/plot.py
+++ doc/plot.py
@@ -7,7 +7,12 @@ media = 0
 desvio = 0
 dados = []
 data = []
-matrix = []
+matrix_N = [[0 for i in range(10)] for j in range(10)]
+count_N = 0
+matrix_T = [[0 for i in range(10)] for j in range(10)]
+count_T = 0
+matrix_P = [[0 for i in range(10)] for j in range(10)]
+count_P = 0
 
 for f in glob.glob('*.txt'):
   arq = open(f, 'r')
@@ -25,12 +30,27 @@ for f in glob.glob('*.txt'):
         desvio += (element-media)**2
       desvio = math.sqrt(desvio/len(dados))
       [N, size] = f.replace('.txt', '').replace('resultados_','').split('_')
-      data = [N] + [size] + [tipo] + [media] + [desvio]
-      matrix = matrix + data
+      data = [tipo] + [size] + [N] + [media] + [desvio]
+      if(data[0] == 'N'):
+        for i in range(0,len(data)):
+          print(i)
+          matrix_N[count_N][i] = data[i]
+          count_N += 1
+      if(data[0] == 'T'):
+        for i in range(1,len(data)):
+          matrix_T[count_T][i] = data[i]
+          count_T += 1
+      if(data[0] == 'P'):
+        for i in range(1,len(data)):
+          matrix_P[count_P][i] = data[i]
+          count_P += 1
+      #print(data)
       media = 0
       desvio = 0
       dados.clear()
       data.clear()
       continue
   arq.close() 
-print(matrix) 
+print(matrix_N)
+print(matrix_T)
+print(matrix_P)
