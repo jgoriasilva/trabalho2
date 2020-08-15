cccf12e69fa78b4dd99728c27024176c5abb714a
João Gória <jgoriasilva@gmail.com>
2020-08-15 17:50:29 -0300

    n sei

--- doc/plot.py
+++ doc/plot.py
@@ -2,17 +2,13 @@ import glob
 import os
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 media = 0
 desvio = 0
 dados = []
 data = []
-matrix_N = [[0 for i in range(10)] for j in range(10)]
-count_N = 0
-matrix_T = [[0 for i in range(10)] for j in range(10)]
-count_T = 0
-matrix_P = [[0 for i in range(10)] for j in range(10)]
-count_P = 0
+out = open("Resultados.txt", "w")
 
 for f in glob.glob('*.txt'):
   arq = open(f, 'r')
@@ -31,26 +27,37 @@ for f in glob.glob('*.txt'):
       desvio = math.sqrt(desvio/len(dados))
       [N, size] = f.replace('.txt', '').replace('resultados_','').split('_')
       data = [tipo] + [size] + [N] + [media] + [desvio]
-      if(data[0] == 'N'):
-        for i in range(0,len(data)):
-          print(i)
-          matrix_N[count_N][i] = data[i]
-          count_N += 1
-      if(data[0] == 'T'):
-        for i in range(1,len(data)):
-          matrix_T[count_T][i] = data[i]
-          count_T += 1
-      if(data[0] == 'P'):
-        for i in range(1,len(data)):
-          matrix_P[count_P][i] = data[i]
-          count_P += 1
       #print(data)
+      out.write(str(data).replace('[', '').replace('\'','').replace(',','').replace(']','') + '\n')
       media = 0
       desvio = 0
       dados.clear()
       data.clear()
-      continue
-  arq.close() 
-print(matrix_N)
-print(matrix_T)
-print(matrix_P)
+  arq.close()
+out.close() 
+
+f = open('Resultados.txt', 'r')
+for line in f:
+  dados = line.split()
+  data = data + dados
+f.close()
+
+tipo = data[0:len(data):5]
+size = data[1:len(data):5]
+N = data[2:len(data):5]
+media = np.array(list(map(float, data[3:len(data):5])))
+deviation = np.array(list(map(float, data[4:len(data):5])))
+
+print(tipo)
+print(size)
+print(N)
+print(media)
+print(deviation)
+
+X = np.arange(24)
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.bar(X + 0.00, data[0], color = 'b', width = 0.25)
+ax.bar(X + 0.25, data[1], color = 'g', width = 0.25)
+ax.bar(X + 0.50, data[2], color = 'r', width = 0.25)
+ax.legend(labels=['N', 'T', 'P'])
